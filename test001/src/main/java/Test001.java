e14926641b37fe9b6cbf4852443ef3a73efa68f0
LittleSquirrel777 <65956712+LittleSquirrel777@users.noreply.github.com>
2021-07-15 19:45:10 +0800

    Update Test001.java

--- test001/src/main/java/Test001.java
+++ test001/src/main/java/Test001.java
@@ -4,6 +4,6 @@ public class Test001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(new Date());
-        System.out.println("wpp");
+        System.out.println("远端仓库001");
     }
 }
