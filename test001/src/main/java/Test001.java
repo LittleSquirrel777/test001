0521c355a40f80c1fecf4ec83318e84bb586203f
littlesquirrel <2436700441@qq.com>
2021-07-15 19:21:25 +0800

    second commit

--- test001/src/main/java/Test001.java
+++ test001/src/main/java/Test001.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 public class Test001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(new Date());
+        System.out.println("wc");
     }
 }
